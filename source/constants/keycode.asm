8da82927aba7e80a303d626f3f6867e3bfc69ee1
ZeroKelvinKeyboard <zerokelvinkeyboard@gmail.com>
2018-08-02 07:08:06 +0930

    Fixed missing file 'keycode.asm'

---
+++ source/constants/keycode.asm
@@ -0,0 +1,11 @@
+%define LEFT_KEY 0x4BE0
+%define RIGHT_KEY 0x4DE0
+%define HOME_KEY 0x47E0
+%define END_KEY 0x4FE0
+%define INSERT_KEY 0x52E0
+%define DELETE_KEY 0x53E0
+%define ESCAPE_KEY 27
+%define ENTER_KEY 13
+%define BACKSP_KEY 8
+
+
